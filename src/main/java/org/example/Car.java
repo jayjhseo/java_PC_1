e1f9cf0a3b586d5d6f0718176f400ee0be7bfd2f
jayjhseo <jayjhseo@gmail.com>
2023-09-20 17:33:40 +0900

    class_01

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Car {
+    void run() {
+        System.out.println(number + "번 자동차가 최고속력 " + speed + "km로 달립니다.");
+    }
+    int speed;
+    int number;
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,15 @@
 package org.example;
 
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-
-        for (int i = 1; i <= 5; i++) {
-
-
-            System.out.println("i = " + i);
-        }
+        Car car1 = new Car();
+        Car car2 = new Car();
+        car1.speed = 230;
+        car2.speed = 210;
+        car1.number = 1;
+        car2.number = 2;
+        car1.run();
+        car2.run();
     }
 }
